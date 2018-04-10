a1bc97e7c330641f5f1818406482392a309a224b
fcastaneda9725 <f.castanedat@gmail.com>
2018-04-10 14:19:00 -0500

    Estilos en los botones de los productos, puesta de infomración en ellas

--- src/app/views/tarjeta-prod/tarjeta-prod.component.ts
+++ src/app/views/tarjeta-prod/tarjeta-prod.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { DataService } from '../../services/data.service';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'tarjeta-prod',

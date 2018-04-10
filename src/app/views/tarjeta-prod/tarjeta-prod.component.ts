0d1f50f9c0379d9d0c2ebc8668ca5aa47e7558e5
fcastaneda9725 <f.castanedat@gmail.com>
2018-04-10 15:36:39 -0500

    Ajustes de trajetas

--- src/app/views/tarjeta-prod/tarjeta-prod.component.ts
+++ src/app/views/tarjeta-prod/tarjeta-prod.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { DataService } from '../../services/data.service';
-import { TitleCasePipe } from '@angular/common';
+import { TitleCasePipe, CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'tarjeta-prod',
